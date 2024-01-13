e03213504936afc102b9688efe50994cf54e4a99
Abraham Tesfaye <dewtesfaye76@gmail.com>
2024-01-13 22:07:31 +0300

    add smart contract sample

--- Algo_Certificate/backend/smart_contracts/algo_cert/contract.py
+++ Algo_Certificate/backend/smart_contracts/algo_cert/contract.py
@@ -1,10 +1,23 @@
 import beaker
 import pyteal as pt
 
-
 app = beaker.Application("algo_cert")
 
 
 @app.external
 def hello(name: pt.abi.String, *, output: pt.abi.String) -> pt.Expr:
     return output.set(pt.Concat(pt.Bytes("Hello, "), name.get()))
+
+
+@app.external
+def opt_in() -> pt.Expr:
+    sender = pt.Txn.sender()
+    already_opted_in = pt.App.globalGet(sender)
+    opt_in_logic = pt.Seq(
+        [
+            pt.Assert(pt.Not(already_opted_in)),
+            pt.App.globalPut(sender, pt.Int(1)),
+            pt.Approve(),
+        ]
+    )
+    return opt_in_logic
